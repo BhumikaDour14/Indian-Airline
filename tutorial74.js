4bbae96fab18de2eeee507045029259cc2ef67db
BhumikaDour14 <bhumikadour14@gmail.com>
2025-04-30 01:34:04 +0530

    Delete tutorial74.js

--- tutorial74.js
+++
@@ -1,5 +0,0 @@
-let Button =document.getElementById("button")
-Button.addEventListener("dblclick", ()=>{
-    //alert("I was clicked, Yahh!!")
-    document.querySelector(".box").innerHTML="<b>I was clicked</b>Yahh!!"
-})
